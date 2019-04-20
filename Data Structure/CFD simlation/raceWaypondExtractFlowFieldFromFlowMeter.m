8aa8ba1ea48084f573939c95e4f25297e71b1d19
giantsda <chenshen@rams.colostate.edu>
2019-04-20 14:35:07 -0600

    minor fix

--- Data Structure/CFD simlation/raceWaypondExtractFlowFieldFromFlowMeter.m
+++ Data Structure/CFD simlation/raceWaypondExtractFlowFieldFromFlowMeter.m
@@ -1,39 +1,54 @@
-path='/home/chen/Desktop/racewayMeasurements/RPM=13.6/Calibration5/';
-files=dir([path 'Region=2*']);
+Caliration=[0.00100051078850230	0.0301863015082328;
+    0.000998257129028534	0.0296510635717292;
+    0.00100061048014776	0.0282036218985373;
+    0.000986641900693   0.051101900619225;
+    0.00100438979767638	0.0408383032671176;
+    ];
+
+
+path='E:\desktop\temp\racewayMeasurements\RPM=13.6\Calibration3\';
+C=Caliration(3,:);
+files=dir([path 'Region=1*']);
+x=55;
+
+
+
 fileNameMatrix=[];
+value_s=[];
 for i=1:length(files)
     filename=files(i).name;
-    fileNameMatrix(i,:)=sscanf(files(i).name,'Region=2x=%fy=%fz=%f').';
+    fileNameMatrix(i,:)=sscanf(files(i).name,'Region=%*dx=%fy=%fz=%f').';
 end
- 
-x_mesh=[ 5    10    15    20    25    30    35    40    45    48];
-y_mesh=[0.0500    0.1000    0.1500];
 
-[x1,y1]=meshgrid(x_mesh,y_mesh*100);
-for i=1:length(y_mesh)
-    for j=1:length(x_mesh)
-        k=find(fileNameMatrix(:,2)==x_mesh(j)&fileNameMatrix(:,3)==y_mesh(i));
+y_mesh=[ 5    10    15    20    25    30    35    40    45  48];
+z_mesh=[0.0500    0.1000    0.1500];
+
+[y1,z1]=meshgrid(y_mesh,z_mesh*100);
+for i=1:length(z_mesh)
+    for j=1:length(y_mesh)
+        k=find(fileNameMatrix(:,1)==x&fileNameMatrix(:,2)==y_mesh(j)&fileNameMatrix(:,3)==z_mesh(i));
         filename=[path files(k).name]
         
         fid = fopen(filename);
-    line=0;
-    while  ~feof(fid)
-        l = fgetl(fid);
-        line=line+1;
-        if ~isempty(l)
-            if strcmp(l(1:6),'Time_:')
-                a = sscanf(l,'Time_:%*f  RPM_1s:%*f  averagedRPM:%f');
+        line=0;
+        while  ~feof(fid)
+            l = fgetl(fid);
+            line=line+1;
+            if ~isempty(l)
+                if strcmp(l(1:6),'Time_:')
+                    a = sscanf(l,'Time_:%*f  RPM_1s:%*f  averagedRPM:%f');
+                end
             end
         end
+        value_s(i,j)=a;
     end
-    a
-    value_s(i,j)=a;
-    end
-end 
+end
 
-value_s=value_s*0.00100438979767638+	0.0408383032671176;
-x1=56-x1;
-pcolor(x1,y1,value_s);
+value_s=value_s*C(1)+C(2);
+% y1=56-y1;
+figure;
+
+pcolor(y1,z1,value_s);
 shading interp;
 axis equal
 colormap jet
--- Data Structure/CFD simlation/racewayAverageVelocityField2.m
+++ Data Structure/CFD simlation/racewayAverageVelocityField2.m
@@ -1,16 +1,21 @@
-path='/home/chen/Desktop/project/20/';
-files=dir([path 'export*']);
-z_mesh=56-[5 10 15 20 25 30 35 40 45 48];
-z_mesh=z_mesh/100;
-y_mesh=[0.05 0.10 0.15];
+path='C:\CFD_second_HHD\racewayOpenfoam\04192019\24\data\';
+filesTotal=dir([path 'export*']);
+y_mesh=56-[5 10 15 20 25 30 35 40 45 48];  %% 56 and 155
+y_mesh=y_mesh/100;
+z_mesh=[0.05 0.10 0.15];
 
-z_mesh=56-linspace(0,56,50);
-z_mesh=z_mesh/100;
-y_mesh=linspace(0,0.2,50);
+% y_mesh=linspace(-56,56,50);
+% y_mesh=y_mesh/100;
+% z_mesh=linspace(0,0.2,50);
 
-[z1,y1]=meshgrid(z_mesh,y_mesh);
+[y1,z1]=meshgrid(y_mesh,z_mesh);
+value_s=y1*0;
 
-value_s=z1*0;
+files=[];  % too much data, read every 15
+for i=1:10:length(filesTotal)
+    files=[files filesTotal(i)];
+end
+
 parfor_progress(length(files)); % Set the total number of files
 parfor i=1:length(files)
     file=[path files(i).name]
@@ -19,15 +24,23 @@ parfor i=1:length(files)
     dataI = importdata(file,delimiterIn,headerlinesIn);
     dataI=dataI.data;
     % cellnumber, x-coordinate,y-coordinate, z-coordinate,x-velocity,y-velocity, z-velocity, w-vof
-    sliceI=dataI(find(abs(dataI(:,2)--0.85)<=1e-5),:);
-    value_i=griddata(sliceI(:,4),sliceI(:,3),abs(sliceI(:,5)),z1,y1);
+    sliceI=dataI(find(abs(dataI(:,2)-0.85)<=1e-5),:);
+    value_i=griddata(sliceI(:,3),sliceI(:,4),abs(sliceI(:,5)),y1,z1);
     value_s=value_s+value_i;
     parfor_progress;
+%% test plot
+%     pcolor(y1,z1,value_i);
+%     shading interp;
+%     axis equal
+%     colormap jet
+%     colorbar
+%     pause();
 end
 parfor_progress(0);
 
-
-pcolor(z1,y1,value_s/length(files))
+meanVelocity=value_s/length(files);
+figure;
+pcolor(y1,z1,meanVelocity)
 shading interp;
 axis equal
 colormap jet
